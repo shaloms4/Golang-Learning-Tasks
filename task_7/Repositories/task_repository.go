961c37f1a2994b897f03c7d5350d734e84fb023e
Shalom Habtamu <shalomhabtamum@gmail.com>
2025-03-30 19:13:15 +0300

    Edit update task logic

--- task_7/Repositories/task_repository.go
+++ task_7/Repositories/task_repository.go
@@ -85,17 +85,35 @@ func (r *taskRepository) Update(ctx context.Context, id string, task *Domain.Tas
 		return err
 	}
 
-	update := bson.M{
-		"$set": bson.M{
-			"title":       task.Title,
-			"description": task.Description,
-			"status":      task.Status,
-			"due_date":    task.DueDate,
-		},
+	// Create an empty map to store fields that need to be updated
+	updateFields := bson.M{}
+
+	// Dynamically add fields to the update map only if they are not empty
+	if task.Title != "" {
+		updateFields["title"] = task.Title
+	}
+	if task.Description != "" {
+		updateFields["description"] = task.Description
+	}
+	if task.Status != "" {
+		updateFields["status"] = task.Status
+	}
+	if task.DueDate.IsZero() { 
+		updateFields["due_date"] = task.DueDate
 	}
 
-	_, err = r.db.Collection(Domain.CollectionTask).UpdateOne(ctx, bson.M{"_id": objID}, update)
-	return err
+	if len(updateFields) > 0 {
+		update := bson.M{
+			"$set": updateFields,
+		}
+
+		// Update the task in the database
+		_, err := r.db.Collection(Domain.CollectionTask).UpdateOne(ctx, bson.M{"_id": objID}, update)
+		return err
+	}
+
+	// If no fields are provided to update, return nil (no update was performed)
+	return nil
 }
 
 func (r *taskRepository) Delete(ctx context.Context, id string) error {
